be4541fa903d2ff9a3a2da52934f4b12170a37c5
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-12-16 13:53:17 +0100

    get announcements for location `Tul`

---
+++ src/routes/station/Tul/+page.server.js
@@ -0,0 +1,45 @@
+import { error } from '@sveltejs/kit';
+
+export const load = async ({ params }) => {
+	console.log(params);
+
+	const announcementsResponse = await fetch('https://api.trafikinfo.trafikverket.se/v2/data.json', {
+		method: 'POST',
+		body: getBody({ location: 'Tul' }),
+		headers: {
+			'Content-Type': 'application/xml',
+			Accept: 'application/json'
+		}
+	});
+	if (!announcementsResponse.ok)
+		throw error(announcementsResponse.status, announcementsResponse.statusText);
+
+	const { RESPONSE } = await announcementsResponse.json();
+	const [announcements] = RESPONSE.RESULT;
+
+	return {
+		announcements
+	};
+};
+
+function getBody({ location }) {
+	return `
+<REQUEST>
+  <LOGIN authenticationkey='${process.env.TRAFIKVERKET_API_KEY}' />
+     <QUERY objecttype='TrainAnnouncement' orderby='AdvertisedTimeAtLocation' schemaversion='1.6'>
+      <FILTER>
+         <AND>
+            <NE name='Canceled' value='true' />
+            <EQ name='Advertised' value='true' />
+            <EQ name='ActivityType' value='Avgang' />
+            <EQ name='LocationSignature' value='${location}' />
+            <OR>
+               <GT name='AdvertisedTimeAtLocation' value='$dateadd(-0:05:00)' />
+               <GT name='EstimatedTimeAtLocation' value='$dateadd(-0:05:00)' />
+            </OR>
+            <LT name='AdvertisedTimeAtLocation' value='$dateadd(0:30:00)' />
+         </AND>
+      </FILTER>
+     </QUERY>
+</REQUEST>`;
+}

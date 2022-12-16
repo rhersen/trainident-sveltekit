cc86c31380d311efd25410f2e255d243c96a1f9e
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-12-16 13:05:48 +0100

    get announcements for location `Tul`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="282">282</a> to read the documentation</p>
-<p>Visit <a href="8743">8743</a> to read the documentation</p>
+<p>Visit <a href="station/Tul">Tul</a> to read the documentation</p>

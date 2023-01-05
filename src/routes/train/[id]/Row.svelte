d69d9fa29ac210a1fd8f3f31e6dea172bb9beac4
Rudolf Hersén <rudolf.hersen@schibsted.com>
2023-01-05 10:07:31 +0100

    delay colors for train

--- src/routes/train/[id]/Row.svelte
+++ src/routes/train/[id]/Row.svelte
@@ -7,11 +7,22 @@
 	import DeviationCell from '$lib/components/DeviationCell.svelte';
 	import LocationCell from './LocationCell.svelte';
 	import CountdownCell from '$lib/components/CountdownCell.svelte';
+	import { differenceInMinutes, parseISO } from 'date-fns';
 
 	export let announcement;
+
+	function getColor() {
+		const delay =
+			announcement.TimeAtLocationWithSeconds &&
+			differenceInMinutes(
+				parseISO(announcement.TimeAtLocationWithSeconds),
+				parseISO(announcement.AdvertisedTimeAtLocation)
+			).toString();
+		if (delay) return delay < 1 ? '1' : delay < 2 ? '2' : delay < 4 ? '4' : delay < 8 ? '8' : '9';
+	}
 </script>
 
-<tr>
+<tr class={`delay-${getColor()}`}>
 	<TrackCell {announcement} />
 	<ShortCell {announcement} />
 	<LocationCell {announcement} />
@@ -21,3 +32,21 @@
 	<CountdownCell {announcement} />
 	<DeviationCell {announcement} />
 </tr>
+
+<style>
+	.delay-1 {
+		background-color: #8f8;
+	}
+	.delay-2 {
+		background-color: #ccc;
+	}
+	.delay-4 {
+		background-color: #ff8;
+	}
+	.delay-8 {
+		background-color: #fc8;
+	}
+	.delay-9 {
+		background-color: #f88;
+	}
+</style>
